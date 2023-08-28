ca9d6c148e1f46709aa915d9fd0bea803077f188
Alex Gustavo Welz <alex.welz6@gmail.com>
2023-08-28 14:33:59 -0300

    ABM project finished

---
+++ CrudNet7MVC-UsuariosABM/CrudNet7MVC-UsuariosABM/CrudNet7MVC-UsuariosABM/Datos/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using CrudNet7MVC_UsuariosABM.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CrudNet7MVC_UsuariosABM.Datos
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        //Agregar modelos acá
+        public DbSet<Contacto> Contacto { get; set; }
+
+    }
+}
---
+++ CrudNet7MVC-UsuariosABM/CrudNet7MVC-UsuariosABM/CrudNet7MVC-UsuariosABM/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CrudNet7MVC_UsuariosABM.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
